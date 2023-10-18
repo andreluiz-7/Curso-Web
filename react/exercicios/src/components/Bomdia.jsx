5e804ad7c3b3c2875c9aa49a90158c13c9eeab1f
andre <andrediasluiz6@gmail.com>
2023-10-18 13:38:38 -0300

    Organizando arquivos do Curso  (DOM, ESnext, JS, Node)

--- react/exercicios/src/components/Bomdia.jsx
+++
@@ -1,6 +0,0 @@
-import React  from 'react'
-
-export default props => [
-        <h1 key='h1'>Bom dia {props.nome}!</h1>,
-        <h2 key='h2'>Show da bola?</h2>
-]
--- react/exercicios/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import ReactDOM from 'react-dom'
-//import Primeiro from './components/Primeiro'
-import Bomdia from "./components/Bomdia"
-
-
-ReactDOM.render(<Bomdia nome = 'Andre'/>, document.getElementById('root'))
-
-
